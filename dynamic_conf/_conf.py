5dc1df05a7388ab1b69d55bf84b5504709396ac3
Noortheen Raja <jnoortheen@gmail.com>
2020-03-10 16:51:59 +0530

    feat: add read support for .env files

--- dynamic_conf/_conf.py
+++ dynamic_conf/_conf.py
@@ -1,24 +1,16 @@
 from __future__ import print_function
 
-from collections import OrderedDict
-from typing import Type
-
-from ._import import import_file
-
-try:
-    reload
-except Exception:
-    from importlib import reload
-
-import importlib
+import logging
 import os
+from typing import Type, Mapping
 
 from six import with_metaclass
-import logging
 
+from ._env import get_env_file_path, reader, writer, DEFAULT_FILE
 
 _UNDEFINED = object()
 REQUIRED = object()  # only for Python2 support
+
 log = logging.getLogger(__file__)
 
 
@@ -28,7 +20,7 @@ class Var(object):
             if not given a default explicitly then this will raise an error.
             Get the given environment variable in following order
                 1. os.environment
-                2. {file_name}.py or .env (if the name ends with .env and set by user)
+                2. {_file_name}.py or .env (if the name ends with .env and set by user)
                 3. default value
         """
         self.name = name
@@ -38,8 +30,11 @@ class Var(object):
     def __get__(self, instance, owner):
         if self.name in os.environ:
             return os.environ[self.name]
-        if self.module and hasattr(self.module, self.name):
-            return getattr(self.module, self.name)
+        if self.module:
+            if isinstance(self.module, Mapping):
+                return self.module[self.name]
+            if hasattr(self.module, self.name):
+                return getattr(self.module, self.name)
         if self.default not in {_UNDEFINED, REQUIRED}:
             return self.default
 
@@ -48,29 +43,6 @@ class Var(object):
                 self.name, get_env_file_path(owner)
             )
         )
-
-
-def get_env_file_path(cls):
-    conf = cls._registry[-1]
-    mod = importlib.import_module(conf.__module__)
-    mod_dir = os.path.dirname(mod.__file__) if hasattr(mod, "__file__") else "."
-    return os.path.join(mod_dir, "{}.py".format(cls.file_name))
-
-
-def import_env_module(cls):
-    # reload os.environ
-    reload(os)
-
-    # an object that supports getattr
-    env_module = None
-    file_path = get_env_file_path(cls)
-    try:
-        env_module = import_file(file_path)
-    except ImportError:
-        log.info(
-            "{} is not found. Getting variables from environment.".format(file_path)
-        )
-    return env_module
 
 
 class ConfigMeta(type):
@@ -88,7 +60,7 @@ class ConfigMeta(type):
             )
 
         if len(cls._registry) > 1:
-            env_module = import_env_module(cls)
+            env_module = reader(cls)
             for attrname, attrvalue in attrs.items():
                 if not attrname.startswith("_"):
                     setattr(cls, attrname, Var(env_module, attrname, default=attrvalue))
@@ -98,58 +70,10 @@ class ConfigMeta(type):
         return cls
 
 
-def _normalize_prefix(default_prefix):
-    prefix = os.environ.get("VARS_PREFIX", default_prefix)
-    vals = OrderedDict()
-    if prefix:
-        for k, val in os.environ.items():
-            if k.startswith(prefix):
-                vals[k.replace(prefix, "")] = val
-    return vals
-
-
-DEFAULT_FILE = "env"
-
-
-def writer(cls, argv):
-    # type: (Config, str) -> OrderedDict
-
-    CONF_FILE = get_env_file_path(cls)
-    if os.path.exists(CONF_FILE):
-        raise Exception(f"Found {CONF_FILE} existing already")
-
-    vals = OrderedDict()
-    vals.update(_normalize_prefix(cls._default_prefix))
-    for k in cls.__dict__.keys():
-        if k in os.environ:
-            vals[k] = os.environ[k]
-
-    for arg in argv:
-        if "=" in arg:
-            k, val = arg.split("=")
-            vals[k] = val
-
-    if vals:
-        log.info(
-            "Writing following keys\n\t"
-            + "\n\t".join(vals.keys())
-            + "\n to "
-            + CONF_FILE
-        )
-        with open(CONF_FILE, "w") as f:
-            f.write(
-                "\n".join(["{} = {}".format(k, repr(val)) for k, val in vals.items()])
-            )
-    else:
-        log.info("Dynamic-Conf: No variables available.")
-
-    return vals
-
-
 class Config(with_metaclass(ConfigMeta)):
-    """singleton to be used for configuring from os.environ and {file_name}.py"""
+    """singleton to be used for configuring from os.environ and {_file_name}.py"""
 
-    file_name = DEFAULT_FILE
+    _file_name = DEFAULT_FILE
     """by default the suffix will be .py unless the file name is changed in the subclass"""
 
     _default_prefix = ""
@@ -163,3 +87,10 @@ class Config(with_metaclass(ConfigMeta)):
             )
 
         return writer(cls._registry[-1], argv)
+
+    @classmethod
+    def get_file_name(cls):
+        if cls._file_name == DEFAULT_FILE:
+            return f"{cls._file_name}.py"
+        else:
+            return cls._file_name
---
+++ dynamic_conf/_env.py
@@ -0,0 +1,107 @@
+"""Envs file types
+
+1. .py
+2. .env
+"""
+import logging
+from _collections import OrderedDict
+
+from ._import import import_file
+
+try:
+    reload
+except Exception:
+    from importlib import reload
+
+import os
+import inspect
+
+log = logging.getLogger(__file__)
+
+DEFAULT_FILE = "env"
+
+
+def get_env_file_path(cls):
+    conf = cls._registry[-1]
+    mod_path = "."
+    try:
+        mod_path = os.path.dirname(inspect.getfile(conf))
+    except TypeError:
+        pass
+    return os.path.join(mod_path, cls.get_file_name())
+
+
+def reader(cls):
+    # reload os.environ
+    reload(os)
+
+    # an object that supports getattr
+    file_path = get_env_file_path(cls)
+    _, ext = os.path.splitext(file_path)
+    if ext == ".py":
+        env_module = import_file(file_path)
+    else:
+        env_module = _read_dotenv(file_path)
+    if not env_module:
+        log.info(
+            "{} is not found. Getting variables from environment.".format(file_path)
+        )
+
+    return env_module
+
+
+def _read_dotenv(file_path):
+    env_vars = {}
+    if os.path.exists(file_path):
+        with open(file_path) as f:
+            for line in f:
+                if (
+                    line.strip().startswith("#") or not line.strip()
+                ):  # skip comments and empty lines
+                    continue
+                key, value = line.strip().split("=", 1)
+                env_vars[key] = value  # Save to a list
+    return env_vars
+
+
+def _normalize_prefix(default_prefix):
+    prefix = os.environ.get("VARS_PREFIX", default_prefix)
+    vals = OrderedDict()
+    if prefix:
+        for k, val in os.environ.items():
+            if k.startswith(prefix):
+                vals[k.replace(prefix, "")] = val
+    return vals
+
+
+def writer(cls, argv):
+    CONF_FILE = get_env_file_path(cls)
+    if os.path.exists(CONF_FILE):
+        raise Exception(f"Found {CONF_FILE} existing already")
+
+    vals = OrderedDict()
+    vals.update(_normalize_prefix(cls._default_prefix))
+    for k in cls.__dict__.keys():
+        if k in os.environ:
+            vals[k] = os.environ[k]
+
+    for arg in argv:
+        if "=" in arg:
+            k, val = arg.split("=")
+            vals[k] = val
+
+    if vals:
+        log.info(
+            "Writing following keys\n\t"
+            + "\n\t".join(vals.keys())
+            + "\n to "
+            + CONF_FILE
+        )
+        with open(CONF_FILE, "w") as f:
+            f.write(
+                "\n".join(["{} = {}".format(k, repr(val)) for k, val in vals.items()])
+            )
+    else:
+        log.info("Dynamic-Conf: No variables available.")
+
+    return vals
